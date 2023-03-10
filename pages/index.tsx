73fc1127bbaabd6a9233ec9a445ddf4ae3aa87cc
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-10 12:26:43 +0100

    Created Footer, Navbar and Meta components. Added page icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
   return (
