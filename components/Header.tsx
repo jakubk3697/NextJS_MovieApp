3f33d89cc92dc03c4106c3d5cb48f216d2a8a273
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-13 11:19:46 +0100

    Created initial toggle button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,18 @@
 import styles from '@/styles/Header.module.scss'
-import {BiCameraMovie} from 'react-icons/bi'
-import Link from 'next/link'
+import {BiCameraMovie} from 'react-icons/bi';
+import Link from 'next/link';
+import { HeaderSearchbar } from './elements/HeaderSearchbar';
+import { ThemeButton } from './elements/ThemeButton';
 
 export const Header = () => {
     return(
-        <header>
-            <Link className="" href={'/'}>
-                <div>
-                    <BiCameraMovie />
-                    <h1>FilmFiesta</h1>
-                </div>
+        <header className="flex items-center p-4">
+            <Link href={'/'} className="flex hover:opacity-90 transition-opacity pr-4">
+                    <BiCameraMovie className="text-4xl mr-1" />
+                    <h1 className="text-3xl font-semibold tracking-wider">Movie App</h1>
             </Link>
+            <HeaderSearchbar />
+            <ThemeButton />
         </header>
     );
 }
---
+++ components/elements/HeaderSearchbar.tsx
@@ -0,0 +1,9 @@
+
+export const HeaderSearchbar = () => {
+    return(
+        <div>
+            <input type="text" placeholder="Write title to search movies..." />
+            <button>Search</button>
+        </div>
+    );
+}
