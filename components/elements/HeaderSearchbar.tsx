e5a67cb834197840d738d11ef168f78f1e1efc99
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-13 12:37:21 +0100

    Updated styling for header

--- components/elements/HeaderSearchbar.tsx
+++ components/elements/HeaderSearchbar.tsx
@@ -1,12 +1,14 @@
 
 export const HeaderSearchbar = () => {
     return(
-        <div className="flex items-center w-full mt-5 md:w-1/2 md:order-1 md:mt-0">
+        <div className="flex items-center w-full font-semibold text-gray-700 bg-gray-200 rounded-lg mt-5 md:w-1/2 md:order-1 md:mt-0">
             <input 
                 type="text" 
                 placeholder="Search movies..." 
-                className="block font-semibold text-gray-700 bg-gray-200 rounded-lg px-2 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 w-full">
+                // write box shadow tailwind version here
+                className="bg-transparent px-2 py-2 rounded-lg w-full">
             </input>
+            <button className="py-2 px-4 rounded-r-lg bg-sky-700 text-white hover:opacity-90 transition-opacity">Search</button>
         </div>
     );
 }
