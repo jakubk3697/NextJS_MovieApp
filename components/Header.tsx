0e6ed96b36cd09d420702eae5e98bcfd16ea755b
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-13 12:02:55 +0100

    Finished theme button styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,13 +6,13 @@ import { ThemeButton } from './elements/ThemeButton';
 
 export const Header = () => {
     return(
-        <header className="flex items-center p-4">
+        <header className="flex items-center p-4 flex-wrap  ">
             <Link href={'/'} className="flex hover:opacity-90 transition-opacity pr-4">
                     <BiCameraMovie className="text-4xl mr-1" />
                     <h1 className="text-3xl font-semibold tracking-wider">Movie App</h1>
             </Link>
+            <ThemeButton/>
             <HeaderSearchbar />
-            <ThemeButton />
         </header>
     );
 }
--- components/elements/HeaderSearchbar.tsx
+++ components/elements/HeaderSearchbar.tsx
@@ -1,9 +1,12 @@
 
 export const HeaderSearchbar = () => {
     return(
-        <div>
-            <input type="text" placeholder="Write title to search movies..." />
-            <button>Search</button>
+        <div className="flex items-center w-full mt-5 md:w-1/2 md:order-1 md:mt-0">
+            <input 
+                type="text" 
+                placeholder="Search movies..." 
+                className="block font-semibold text-gray-700 bg-gray-200 rounded-lg px-2 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 w-full">
+            </input>
         </div>
     );
 }
