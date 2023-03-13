b7cbbfa77accdf9bc3aa9cbd9cb364d6e7a6747e
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-13 01:02:44 +0100

    Created SSG structure for main and subpages. Added styling in tailwind.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
-import styles from '@/styles/Footer.module.scss'
-
 export const Footer = () => {
     return (
-        <footer className={styles.footer}>
-            <p className={styles.text}>COPYRIGHT© 2023 FilmFiesta - Jakub Kaminski </p>
+        <footer className="flex justify-center items-center absolute bottom-0 w-full h-10 opacity-90 tracking-widest bg-black">
+            <p>COPYRIGHT© 2023 FilmFiesta - Jakub Kaminski </p>
+            <div>
+                {/* <a href="https://www.linkedin.com/in/jakub-kaminski-0b1b1b1b1/" target="_blank" rel="noreferrer">
+                    <img src="/linkedin.svg" alt="LinkedIn" className="h-5 mx-2" />
+                </a> */}
+            </div>
         </footer>
     )
 }
