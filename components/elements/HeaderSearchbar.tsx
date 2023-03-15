17add722a0a3f2b3f540284ca50c4dddb84b1fee
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-15 12:56:01 +0100

    Refactored tailwind style structure

--- components/elements/HeaderSearchbar.tsx
+++ components/elements/HeaderSearchbar.tsx
@@ -1,13 +1,13 @@
 
 export const HeaderSearchbar = () => {
     return(
-        <div className="relative flex items-center w-full font-semibold text-gray-700 bg-gray-200 rounded-lg mt-5 md:w-1/2 md:order-1 md:mt-0">
+        <div className="relative flex items-center w-full mt-5 font-semibold text-gray-700 bg-gray-200 rounded-lg md:w-1/2 md:order-1 md:mt-0">
             <input 
                 type="text" 
                 placeholder="Search movies..." 
-                className="bg-transparent px-2 py-2 rounded-lg w-full pr-28 text-sm">
+                className="w-full pr-28 py-2 pl-2 bg-transparent rounded-lg text-sm">
             </input>
-            <button className="absolute top-0 right-0 bottom-0 py-2 px-4 rounded-r-lg bg-sky-700 text-white hover:opacity-90 transition-opacity text-sm">Search</button>
+            <button className="absolute top-0 right-0 bottom-0 py-2 px-4 rounded-r-lg bg-sky-700 text-sm text-white hover:opacity-90 transition-opacity ">Search</button>
         </div>
     );
 }
--- pages/movie/[id]/index.tsx
+++ pages/movie/[id]/index.tsx
@@ -25,7 +25,7 @@ export default function MovieDetails(params: staticParams) {
     return (
             <div className="container my-12 px-4 bg-black bg-opacity-30">
                 <div className="flex flex-col md:flex-row items-center">
-                    <div className="w-full md:w-1/3 mb-4 md:mb-0">
+                    <div className="w-full mb-4 md:w-1/3 md:mb-0">
                         <Image
                             src={moviePosterUrl}
                             alt={movie.title}
@@ -35,18 +35,18 @@ export default function MovieDetails(params: staticParams) {
                         />
                     </div>
                     <div className="w-full md:w-2/3 md:pl-8">
-                        <h1 className="text-3xl font-bold mb-2">{movie.title}</h1>
-                        <p className="text-gray-400 mb-4">
+                        <h1 className="mb-2 text-3xl font-bold">{movie.title}</h1>
+                        <p className="mb-4 text-gray-400">
                            {movie.overview}
                         </p>
-                        <button className="flex items-center bg-red-500 text-white px-4 py-2 rounded-md">
+                        <button className="flex items-center px-4 py-2 rounded-md bg-red-500 text-white">
                             <BsBookmarkStar className="h-5 w-5 mr-2" />
                             Add to Favorites
                         </button>
                     </div>
                 </div>
                 <div className="my-8">
-                    <h2 className="text-2xl font-bold mb-4">Actors</h2>
+                    <h2 className="mb-4 text-2xl font-bold">Actors</h2>
                     <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
                         {initialActors.map((actor: any) => {
                             return (
@@ -58,7 +58,7 @@ export default function MovieDetails(params: staticParams) {
                                         height={225}
                                         className="rounded-md"
                                     />
-                                    <p className="text-lg font-semibold my-2">{actor.name}</p>
+                                    <p className="my-2 text-lg font-semibold">{actor.name}</p>
                                     <p className="text-gray-300">{actor.character}</p>
                                 </div>
                             )
@@ -66,21 +66,21 @@ export default function MovieDetails(params: staticParams) {
                     </div>
                 </div>
                 <div className="my-8">
-                    <h2 className="text-2xl font-bold mb-4">Reviews</h2>
-                    <div className="flex flex-col md:flex-row items-center justify-between">
-                        <div className="w-full md:w-1/2 mb-4 md:mb-0">
-                            <div className="bg-white rounded-lg shadow-md p-4">
-                                <p className="text-gray-500 mb-2">Posted by User1234 <span className="font-bold text-red-500">{`<--`}Here will be proper user from Database*</span></p>
-                                <p className="text-lg font-semibold mb-2">Amazing Movie!</p>
+                    <h2 className="mb-4 text-2xl font-bold">Reviews</h2>
+                    <div className="flex flex-col items-center justify-between md:flex-row">
+                        <div className="w-full mb-4 md:w-1/2 md:mb-0">
+                            <div className="p-4 bg-white rounded-lg shadow-md">
+                                <p className="mb-2 text-gray-500">Posted by User1234 <span className="font-bold text-red-500">{`<--`}Here will be proper user from Database*</span></p>
+                                <p className="mb-2 text-lg font-semibold">Amazing Movie!</p>
                                 <p className="text-gray-500">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tortor finibus, elementum lacus vel, tempor urna. In vel augue eu quam malesuada interdum. Suspendisse potenti. Donec molestie mauris vel sapien suscipit malesuada. In hac habitasse platea dictumst. Suspendisse non leo non nulla convallis egestas. Nulla vel malesuada magna. Pellentesque aliquet erat nunc, nec tristique quam auctor id. Nunc ac vestibulum urna. Sed imperdiet, lacus in vulputate finibus, lectus sapien molestie velit, vel sagittis augue ex vitae felis. Donec quis luctus tellus. Integer nec metus tellus. Sed suscipit tellus et arcu bibendum, sit amet fermentum nisi facilisis.
                                 </p>
                             </div>
                         </div>
-                        <div className="w-full md:w-1/2 mb-4 md:mb-0">
-                            <div className="bg-white rounded-lg shadow-md p-4">
-                                <p className="text-gray-500 mb-2">Posted by User456 <span className="font-bold text-red-500">{`<--`}Here will be proper user from Database</span></p>
-                                <p className="text-lg font-semibold mb-2">Not my cup of tea</p>
+                        <div className="w-full mb-4 md:w-1/2 md:mb-0">
+                            <div className="p-4 bg-white rounded-lg shadow-md">
+                                <p className="mb-2 text-gray-500">Posted by User456 <span className="font-bold text-red-500">{`<--`}Here will be proper user from Database</span></p>
+                                <p className="mb-2 text-lg font-semibold">Not my cup of tea</p>
                                 <p className="text-gray-500">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tortor finibus, elementum lacus vel, tempor urna. In vel augue eu quam malesuada interdum. Suspendisse potenti. Donec molestie mauris vel sapien suscipit malesuada. In hac habitasse platea dictumst. Suspendisse non leo non nulla convallis egestas. Nulla vel malesuada magna. Pellentesque aliquet erat nunc, nec tristique quam auctor id. Nunc ac vestibulum urna. Sed imperdiet, lacus in vulputate finibus, lectus sapien molestie velit, vel sagittis augue ex vitae felis. Donec quis luctus tellus. Integer nec metus tellus. Sed suscipit tellus et arcu bibendum, sit amet fermentum nisi facilisis.
                                 </p>
