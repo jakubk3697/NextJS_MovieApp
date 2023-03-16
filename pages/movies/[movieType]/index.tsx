a50464b00b71d7d6cd8779b773e1a3a1442bbb39
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-16 14:48:51 +0100

    Adjusted load button

--- pages/movies/[movieType]/index.tsx
+++ pages/movies/[movieType]/index.tsx
@@ -1,15 +1,15 @@
 import { Movies } from '@/types';
-import { MovieCard } from '@/components/MovieCard'; 
+import { MovieCard } from '@/components/MovieCard';
 import { reactQueryFetchMovies, fetchMovies } from '@/API/moviedbAPI';
 import { useState } from 'react';
 import { useInfiniteQuery } from 'react-query';
 import { useRouter } from 'next/router';
 
 interface propsContext {
-   params: { movieType: string };
+    params: { movieType: string };
 }
 
-export default function MoviesPage({movies}:{movies: Movies})  {
+export default function MoviesPage({ movies }: { movies: Movies }) {
     const router = useRouter();
     const { movieType } = router.query;
 
@@ -32,31 +32,35 @@ export default function MoviesPage({movies}:{movies: Movies})  {
 
     const movieData = data ? data.pages.flatMap((page) => page.results) : movies;
 
-    return(
+    return (
         <>
             <section className="grid grid-cols-1 px-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
                 {movieData.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
+                    <MovieCard key={movie.id} movie={movie} />
                 ))}
             </section>
             <button
-                        onClick={() => fetchNextPage()}
-                        disabled={!hasNextPage || isFetchingNextPage}
-                    >
-                        {isFetchingNextPage
-                            ? 'Loading more...'
-                            : hasNextPage
-                                ? 'Load More'
-                                : 'Nothing more to load'}
-                    </button>
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+                className="block mx-auto mt-10 px-5 py-2 text-lg font-medium rounded-md text-white bg-sky-700 focus:outline-none focus:ring-1 focus:ring-offset-2 hover:opacity-95 disabled:opacity-50"
+            >
+                {isFetchingNextPage
+                    ? 'Loading more...'
+                    : hasNextPage
+                        ? 'Load More'
+                        : 'Nothing more to load'}
+            </button>
+
+
+
         </>
-    );    
+    );
 }
 
 export async function getStaticProps(context: propsContext) {
-    const {movieType} = context.params;
+    const { movieType } = context.params;
     const movies = await fetchMovies(movieType, 1);
-    
+
     return {
         props: {
             movies,
@@ -66,13 +70,13 @@ export async function getStaticProps(context: propsContext) {
 }
 
 export async function getStaticPaths() {
-    
+
     return {
         paths: [
-            {params: {movieType: 'popular'}},
-            {params: {movieType: 'top_rated'}},
-            {params: {movieType: 'upcoming'}},
-            {params: {movieType: 'now_playing'}},
+            { params: { movieType: 'popular' } },
+            { params: { movieType: 'top_rated' } },
+            { params: { movieType: 'upcoming' } },
+            { params: { movieType: 'now_playing' } },
         ],
         fallback: false,
     }
