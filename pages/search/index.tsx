5da8def9195a9e1d6a7275140a5b949ce6a2aecc
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-16 17:04:10 +0100

    Initial searchbar works

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,23 +1,37 @@
 import { useRouter } from 'next/router';
 import { fetchMoviesByTitle } from '@/API/moviedbAPI';
 import { useQuery } from 'react-query';
+import { MovieCard } from '@/components/MovieCard';
+import { Movie } from '@/types';
+import { Loader } from '@/components/elements/Loader';
 
 function SearchPage() {
     const router = useRouter();
     const { query }: any = router.query;
-    
-    const { data, isLoading, isError } = useQuery(
+
+    const { data, isFetching, isError } = useQuery(
         ['movies', { query }],
         () => fetchMoviesByTitle(query),
         {
             enabled: !!query,
         }
     )
-    return (
-        <div>
-            <h1>Search Results for {query}</h1>
 
-        </div>
+    if (isFetching) return <Loader/>
+
+    if (isError) return <p>Something went wrong</p>
+    
+    
+    const movies: Movie[] = data?.results;
+
+    return (
+        <>
+            <section className="px-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+                {movies.map((movie: Movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </section>
+        </>
     )
 }
 
