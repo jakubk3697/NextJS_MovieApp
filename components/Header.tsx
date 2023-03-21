5e4523ae3dfd074fea8bc7d8b813cf4c14101958
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-21 12:47:35 +0100

    Finished initial theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
-import styles from '@/styles/Header.module.scss'
 import {BiCameraMovie} from 'react-icons/bi';
 import Link from 'next/link';
 import { HeaderSearchbar } from './elements/HeaderSearchbar';
 import { ThemeButton } from './elements/ThemeButton';
+import { ChangeEventHandler } from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from '@/contexts/theme';
 
 /**
  * @description It renders the header component with the logo, searchbar and theme button. 
@@ -10,14 +12,18 @@ import { ThemeButton } from './elements/ThemeButton';
  * @description Theme button can be used to toggle between light and dark theme.
  * @returns The header component with the logo, searchbar and theme button.
  */
-export const Header = () => {
+export const Header = ({handleTheme}: {handleTheme: ChangeEventHandler<HTMLInputElement>}) => {
+    const theme = useContext(ThemeContext);
+    const themeBgColor = theme === 'dark' ? 'bg-gray-black bg-opacity-50' : 'bg-gray-200 bg-opacity-80';
+    
     return(
-        <header className="sticky top-0 bg-black bg-opacity-70 z-10 flex items-center flex-wrap p-4">
+        <header className={`${themeBgColor} sticky top-0 z-10 flex items-center flex-wrap p-4 rounded-xl`}>
             <Link href={'/'} className="flex pr-4 hover:opacity-90 transition-opacity">
                     <BiCameraMovie className="mr-1 text-4xl" />
-                    <h1 className="text-3xl font-semibold tracking-wider">Movie App</h1>
+                    <h1 className={ `${theme === 'dark' ? 'text-gray-300' : 'text-black'}
+                        text-3xl font-semibold tracking-wider`}>Movie App</h1>
             </Link>
-            <ThemeButton/>
+            <ThemeButton handleTheme={handleTheme}/>
             <HeaderSearchbar />
         </header>
     );
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,10 @@
-import { ReactNode } from "react"
-import {Navbar} from "./Navbar"
-import {Footer} from "./Footer"
-import {Meta} from "./Meta"
-import { Header } from "./Header"
+import { ReactNode } from "react";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
+import { Meta } from "./Meta";
+import { Header } from "./Header";
+import { ThemeContext } from "@/contexts/theme";
+import { useState } from "react";
 
 type LayoutProps = {
     children: ReactNode
@@ -13,17 +15,25 @@ type LayoutProps = {
  * @returns The layout component with the header, navbar, main and footer components
  */
 export default function Layout({ children }: LayoutProps) {
+    const [theme, setTheme] = useState('dark');
+
+  const handleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+
     return (
         <>
             <Meta />
-            <div className="container mx-auto min-h-full">
-                <Header />
-                <Navbar/>
-                <main>
-                    {children}
-                </main>
-            </div>
+            <ThemeContext.Provider value={theme}>
+                <div className="container mx-auto min-h-full">
+                    <Header handleTheme={handleTheme}/>
+                    <Navbar />
+                    <main>
+                        {children}
+                    </main>
+                </div>
                 <Footer />
+            </ThemeContext.Provider>
         </>
     )
 }
