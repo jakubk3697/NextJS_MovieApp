f843e8f78a0b0255906da39bbbd80ea324366d13
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-24 16:50:07 +0100

    Fixed problem with AI serach

--- pages/api/post/openai/generate-titles.tsx
+++ pages/api/post/openai/generate-titles.tsx
@@ -17,11 +17,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       const response = completion.data.choices[0].text || "Nothing found.";
 
-      res.status(200).json({ text: response });
-      res.status(400).json({ text: "No prompt provided." });
-      res.status(500).json({ text: "Something went wrong." });
+      return res.status(200).json({ text: response });
     } catch (err) {
       res.status(500).json({ message: `Could not get comments - Error ${err}` });
+    } finally {
+     return res.end();
     }
   }
 };
